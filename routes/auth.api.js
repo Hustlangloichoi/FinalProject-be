0b51f66e568d01fb2d78e879bfbf88e16c2c671e
Công Minh Hoàng <420ent.@192.168.1.4.non-exists.ptr.local>
2025-04-23 23:20:43 +0700

    3rd edited

--- routes/auth.api.js
+++ routes/auth.api.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const { registerUser, loginUser } = require("../controllers/authController");
-const {
-  validateRequest,
-} = require("../middlewares/validationRequest.middleware");
+const validateRequest = require("../middlewares/validationRequest.middleware");
 const { authSchemas } = require("../validationSchemas/validationSchemas");
 
 const router = express.Router();
