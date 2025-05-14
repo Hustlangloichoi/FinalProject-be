c317846dfde7263c3396b01836ed985850e35dd1
Công Minh Hoàng <420ent.@192.168.1.21.non-exists.ptr.local>
2025-05-14 15:48:38 +0700

    final update on CORS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors({
-  origin: "http://localhost:3000",
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }));
 
